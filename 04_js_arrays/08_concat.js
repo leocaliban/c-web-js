8f04dd117d7a54dfadce9efe34fa2be663cf9865
Leocaliban <leocaliban@gmail.com>
2019-10-17 08:05:20 -0300

    Concat

---
+++ 04_js_arrays/08_concat.js
@@ -0,0 +1,7 @@
+const homens = ['Jack', 'James'];
+const mulheres = ['Kim', 'Kelly'];
+let todos = homens.concat(mulheres);
+console.log(todos);
+
+todos = todos.concat('Michelle');
+console.log(todos);
