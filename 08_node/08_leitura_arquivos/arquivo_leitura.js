36360dcbf3d2852465fc4086f72084c68f96be5e
Leocaliban <leocaliban@gmail.com>
2019-10-19 16:21:25 -0300

    Leitura de arquivos e diretórios

---
+++ 08_node/08_leitura_arquivos/arquivo_leitura.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const caminho = __dirname + '/arquivo.json';
+
+// Sincrono
+const conteudoSync = fs.readFileSync(caminho, 'utf-8');
+console.log(conteudoSync);
+
+// Assincrono
+fs.readFile(caminho, 'utf-8', (error, conteudo) => {
+    const config = JSON.parse(conteudo);
+    console.log(`${config.db.host}:${config.db.port}`);
+});
+
+fs.readdir(__dirname, (error, arquivos) => {
+    console.log('Conteúdo da pasta...');
+    console.log(arquivos);
+})
